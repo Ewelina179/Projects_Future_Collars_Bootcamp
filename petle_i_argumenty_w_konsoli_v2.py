62ce4286b1a70309df8fcecb8e8b26c8bd17ea1e
Ewelina179 <ewelinadalek@gmail.com>
2021-08-23 22:06:32 +0200

    changes in condition where how is weight == 0

--- petle_i_argumenty_w_konsoli_v2.py
+++ petle_i_argumenty_w_konsoli_v2.py
@@ -3,16 +3,16 @@ number_of_packages = 0 # całkowita liczba wysłanych paczek
 whole_weight = 0 # liczba wysłanych kilogramów
 current_package = 0 # bieżąca paczka - licznik wagi
 lightest_packages = 20
-empty = number_of_packages * 20 - whole_weight # liczba "pustych" kilogramów
 current_number_of_element = 0
 
 while current_number_of_element <= how_much_elements:
     
     how_is_weight=int(input("Podaj masę towaru: "))
     if how_is_weight==0:
-        number_of_packages += 1
-        whole_weight += current_package
-        if lightest_packages > current_package:
+        if current_package != 0:
+            number_of_packages += 1
+            whole_weight += current_package
+            if lightest_packages > current_package:
                 lightest_packages = current_package
         print("Zadanie zakończono. ")
         break
@@ -35,22 +35,19 @@ while current_number_of_element <= how_much_elements:
             current_package += how_is_weight
             whole_weight += current_package
             current_number_of_element += 1
-            print(current_number_of_element)
             current_package=0
             if current_number_of_element == how_much_elements:
                 break
-        elif current_package < 20:  #tu poprawka do ostatniego - empty bo nie uwzględniło 
+        elif current_package < 20:
             current_package += how_is_weight
             current_number_of_element += 1
-            print(current_number_of_element)
             if current_number_of_element == how_much_elements:
                 whole_weight += current_package
                 number_of_packages += 1
                 if lightest_packages > current_package:
                     lightest_packages = current_package
-                print(whole_weight)
-                print(empty)  # empty nie działa
                 break
 
+empty = number_of_packages * 20 - whole_weight # liczba "pustych" kilogramów
 print(f"Liczba paczek wysłanych {number_of_packages}, liczba wysłanych kg {whole_weight}, najlżejsza paczka wazyła {lightest_packages} kg. Brak optymalnego pakowania spowodował niezapełnienie przestrzeni, która mogłaby ważyć po wykorzystaniu {empty} kg.")
 
