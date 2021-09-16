55f20b511ad39046f071d6d759d00717d597ba99
Ewelina179 <ewelinadalek@gmail.com>
2021-09-16 10:13:39 +0200

    added exception

--- reader/reader.py
+++ reader/reader.py
@@ -1,37 +1,31 @@
 import sys
+import os
 import csv
-
 
 file = sys.argv[1]
 file_to_write = sys.argv[2]
-lst = []
 to_change = sys.argv[3:]
+lst = []
+lst_to_change=[]
 
-n=[]
 for el in to_change:
     b = el.split(",")
     c = [int(x) for x in b[:-1]]
     d = c[0], c[1], b[2]
-    n.append(d)
-print(n)
+    lst_to_change.append(d)
 
+try:
+    with open(file, newline="") as f:
+        reader = csv.reader(f)
+        for line in reader:
+            lst.append(line)
 
-# reader.py <src> <dst> <change1> <change2> ...
+    for el in lst_to_change:
+        lst[el[0]][el[1]]= el[2]
 
-
-with open(file, newline="") as f:
-	reader = csv.reader(f)
-	for line in reader:
-		lst.append(line)
-print(lst)
-
-
-for el in n:
-    lst[el[0]][el[1]]= el[2]
-
-print(lst)
-
-with open(file_to_write, 'w', newline="") as f: 
-    csvwriter = csv.writer(f) 
-    csvwriter.writerows(lst)
-
+    with open(file_to_write, 'w', newline="") as f: 
+        csvwriter = csv.writer(f) 
+        csvwriter.writerows(lst) 
+        
+except:
+    print(f"Files in current directory: {os.listdir(os.curdir)}")
