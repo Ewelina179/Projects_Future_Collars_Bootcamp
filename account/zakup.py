596e1de7fec4a7aed9984c916c112448fa7d89ea
Ewelina179 <ewelinadalek@gmail.com>
2021-10-16 13:06:30 +0200

    changes in my_func

--- account/zakup.py
+++ account/zakup.py
@@ -2,19 +2,9 @@ import sys
 from acc.accountant import Manager
 
 manager = Manager(sys.argv[2:], file = sys.argv[1])
-print(manager.variables)
 
 @manager.assign("zakup")
-def my_func():
-    with open(sys.argv[1], "r") as fd:
-        products = {}
-        line = fd.readline()
-        sline = line.split(";")
-        main_saldo = float(sline[1])
-        for line in fd.readlines():
-            splitted_line = line.split(";")
-            products[splitted_line[0]] = {"amount": int(splitted_line[1]), "price": float(splitted_line[2])}
+def my_func(manager):
+    print(f"Saldo wynosi: {manager.saldo}; stan magazynu: {manager.products}.")
 
-    print(f"Saldo wynosi: {main_saldo}; stan magazynu: {products}.") # tu niech odczyta z pliku i printować bieżący stan
-
-my_func()
+my_func(manager)
