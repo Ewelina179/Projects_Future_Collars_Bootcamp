8fc0cb45387e7af37aa58e494efcfa5979cc82ab
Ewelina179 <ewelinadalek@gmail.com>
2021-11-15 22:04:20 +0100

    changed views

--- flask_app_with_orm/my_app/app.py
+++ flask_app_with_orm/my_app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_migrate import Migrate
+from flask_wtf import form
 from models import Saldo, Product, History, Manager
 from db import db
 from forms import SaldoForm, PurchaseForm, SaleForm
@@ -17,36 +18,53 @@ def home():
     formSaldo = SaldoForm()
     formSale = SaleForm()
     formPurchase = PurchaseForm()
-    z = db.session.query(Saldo).filter(Saldo.value).order_by((Saldo.date.desc())).first()
-    #saldo = Saldo() coś nie działa ;p
-    saldo = z.value
+    z = db.session.query(Saldo).filter(Saldo.value).order_by(Saldo.date.desc()).first()
+    saldo = Saldo().__show_actual_saldo__()
+    #saldo = z.value
+    #saldo = Saldo(value=1000)
+    #db.session.add(saldo)
+    #db.session.commit()
+    #print(saldo.value)
     #print(Saldo())
     #products = Product().show_products()
     product = db.session.query(Product).all()
-    for el in product:
-        print(el.amount)
 
-    if formSaldo.validate_on_submit():
-        manager.change_saldo(formSaldo.value.data, formSaldo.comment.data)
-        return redirect(url_for('home'))
-    elif formSale.validate_on_submit():
-        manager.sale(formSale.name.data, formSale.price.data, formSale.amount.data)
-        return redirect(url_for('home'))
-    elif formPurchase.validate_on_submit():
-        manager.purchase(formPurchase.name.data, formPurchase.price.data, formPurchase.amount.data)
-        return redirect(url_for('home'))
-    
-    return render_template("index.html", saldo=saldo, magazyn=product, formSale=formSale, formPurchase=formPurchase, formSaldo=formSaldo)
+    magazyn = Product().show_products()
+
+    product = db.session.query(Product).filter(Product.name=="P1").first()
+    print(product.amount)
+    #product.amount=10
+    #db.session.add(product)
+    #db.session.commit()
+    #print(product)
+
+    if request.method == 'POST':
+        if formSaldo.validate_on_submit():
+            print('saldo')
+            manager.change_saldo(formSaldo.value.data, formSaldo.comment.data)
+            flash(formSaldo.errors)
+            return redirect(url_for('home'))
+        if formPurchase.validate_on_submit():
+            print("p")
+            manager.purchase(formPurchase.name.data, formPurchase.price.data, formPurchase.amount.data)
+            flash(formPurchase.errors)
+            return redirect(url_for('home'))
+        if formSale.validate_on_submit():
+            print("s")
+            manager.sale(formSale.name.data, formSale.price.data, formSale.amount.data)
+            flash(formSale.errors)
+            return redirect(url_for('home'))
+            
+    return render_template("index.html", saldo=saldo, product=magazyn, formSale=formSale, formPurchase=formPurchase, formSaldo=formSaldo)
 
 @app.route('/history', defaults={'line_from': None, 'line_to': None})
 @app.route('/history/<int:line_from>/<int:line_to>')
 def history(line_from, line_to):
     history = History()
-    magazyn = Manager()
     if line_from and line_to:
-        logs = magazyn.logs[(line_from-1):(line_to)]
+        logs = history.show_history_from_to(line_from, line_to)
     else:
-        logs = magazyn.logs
+        logs = history.show_history()
     return render_template("history.html", logs=logs)
 
 if __name__ == '__main__':
