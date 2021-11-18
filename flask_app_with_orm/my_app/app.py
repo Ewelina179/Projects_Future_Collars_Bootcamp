082084449c679de0a94b7bfb71bb10c0b3c71d3f
Ewelina179 <ewelinadalek@gmail.com>
2021-11-18 06:56:25 +0100

    changed if statements in home view

--- flask_app_with_orm/my_app/app.py
+++ flask_app_with_orm/my_app/app.py
@@ -12,49 +12,33 @@ app.config['SECRET_KEY'] = "SECRET_KEY"
 db.init_app(app)
 migrate = Migrate(app, db)
 manager = Manager()
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     formSaldo = SaldoForm()
     formSale = SaleForm()
     formPurchase = PurchaseForm()
-    z = db.session.query(Saldo).filter(Saldo.value).order_by(Saldo.date.desc()).first()
-    saldo = Saldo().__show_actual_saldo__()
-    #saldo = z.value
-    #saldo = Saldo(value=1000)
-    #db.session.add(saldo)
-    #db.session.commit()
-    #print(saldo.value)
-    #print(Saldo())
-    #products = Product().show_products()
-    product = db.session.query(Product).all()
-
+    saldo = Saldo().show_actual_saldo()
     magazyn = Product().show_products()
-
-    product = db.session.query(Product).filter(Product.name=="P1").first()
-    print(product.amount)
-    #product.amount=10
-    #db.session.add(product)
-    #db.session.commit()
-    #print(product)
 
     if request.method == 'POST':
         if formSaldo.validate_on_submit():
             print('saldo')
             manager.change_saldo(formSaldo.value.data, formSaldo.comment.data)
+            return redirect(url_for('home'))
+        else:
             flash(formSaldo.errors)
-            return redirect(url_for('home'))
-        if formPurchase.validate_on_submit():
-            print("p")
-            manager.purchase(formPurchase.name.data, formPurchase.price.data, formPurchase.amount.data)
-            flash(formPurchase.errors)
-            return redirect(url_for('home'))
         if formSale.validate_on_submit():
             print("s")
             manager.sale(formSale.name.data, formSale.price.data, formSale.amount.data)
-            flash(formSale.errors)
             return redirect(url_for('home'))
-            
+        else:
+            flash(formSale.errors)
+        if formPurchase.validate_on_submit():
+            print("p")
+            manager.purchase(formPurchase.namep.data, formPurchase.pricep.data, formPurchase.amountp.data)
+            return redirect(url_for('home'))
+        else:
+            flash(formPurchase.errors)
     return render_template("index.html", saldo=saldo, product=magazyn, formSale=formSale, formPurchase=formPurchase, formSaldo=formSaldo)
 
 @app.route('/history', defaults={'line_from': None, 'line_to': None})
